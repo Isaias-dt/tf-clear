b8f92f14e4980bc1abce6b50b7aac845def82ca4
Isaias-dt <isaias-dt@hotmail.com>
2021-09-09 21:22:04 -0300

    Added project start

---
+++ ClearFileTemp/Entities/FilesAndFolders.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace ClearFileTemp.Entities
+{
+    class FilesAndFolders
+    {
+        public string PathFolder { get; set; }
+        private IEnumerable<string> _paths;
+
+        public FilesAndFolders(string pathFolder)
+        {
+            PathFolder = pathFolder;
+            _paths = Directory.EnumerateFileSystemEntries(pathFolder);
+        }
+
+        public void DeleteAllPath()
+        {
+            NotFoundPath();
+            if (!IsFolderEmpty())
+            {
+                foreach (string p in _paths)
+                {
+                    if (Directory.Exists(p))
+                    {
+                        try
+                        {
+                            Directory.Delete(p, true);
+                            Console.WriteLine("Deletado: " + p);
+                        }
+                        catch (IOException e)
+                        {
+                            Console.WriteLine();
+                            Console.WriteLine($"Error ao deletar este Pasta!");
+                            Console.WriteLine($"Error: {e.Message}");
+                            Console.WriteLine();
+                            continue;
+                        }
+                    }
+                    else
+                    {
+                        try
+                        {
+                            File.Delete(p);
+                            Console.WriteLine("Deletado: " + p);
+                        }
+                        catch (IOException e)
+                        {
+                            Console.WriteLine();
+                            Console.WriteLine($"Error ao deletar este arquivo!");
+                            Console.WriteLine($"Error: {e.Message}");
+                            Console.WriteLine();
+                            continue;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine($"Pasta Vazia path({PathFolder})");
+            }
+        }
+
+        public void NotFoundPath()
+        {
+            if (!Directory.Exists(PathFolder))
+            {
+                throw new DirectoryNotFoundException($"Este path({PathFolder}) não existe!");
+            }
+        }
+
+        public bool IsFolderEmpty()
+        {
+            DirectoryInfo dirInf = new DirectoryInfo(PathFolder);
+            return dirInf.GetFiles().Length == 0 && dirInf.GetDirectories().Length == 0;
+        }
+    }
+}
---
+++ ClearFileTemp/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using ClearFileTemp.Entities;
+
+namespace ClearFileTemp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string sysPath = Environment.GetFolderPath(Environment.SpecialFolder.Windows);
+                // temp folder of system and local
+                string localTempPath = Path.GetTempPath();
+                string sysPrefetchPath = sysPath + @"\Prefetch";
+                string sysTempPath = sysPath + @"\Temp";
+
+                FilesAndFolders fifoLocalTemp = new FilesAndFolders(localTempPath);
+                FilesAndFolders fifoSysPrefetch = new FilesAndFolders(sysPrefetchPath);
+                FilesAndFolders fifoSysTemp = new FilesAndFolders(sysTempPath);
+
+                Console.Write("]".PadRight(10, '*'));
+                Console.Write("> " + localTempPath.ToUpper() + " <");
+                Console.WriteLine("".PadLeft(10, '*') + "[");
+                fifoLocalTemp.DeleteAllPath();
+
+                Console.Write("]".PadRight(10, '*'));
+                Console.Write("> " + localTempPath.ToUpper() + " <");
+                Console.WriteLine("".PadLeft(10, '*') + "[");
+                fifoSysPrefetch.DeleteAllPath();
+
+                Console.Write("]".PadRight(10, '*'));
+                Console.Write("> " + localTempPath.ToUpper() + " <");
+                Console.WriteLine("".PadLeft(10, '*') + "[");
+                fifoSysTemp.DeleteAllPath();
+
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred: ");
+                Console.WriteLine(e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+        }
+    }
+}
